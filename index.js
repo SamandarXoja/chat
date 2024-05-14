fd70fef3e283df7bee0e6145bd9fdf084d851809
Samandar99 <laziz.go99@gmail.com>
2024-05-15 04:55:06 +0500

    change soket io

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ mongoose.connect('mongodb+srv://samandarsaidahmadov98:8787172ss@cluster0.soqylcu
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+    transports: ['websocket']
+});
 const PORT = process.env.PORT || 5555;
 app.use(cors());
 
