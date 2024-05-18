f2cd0780de6ffb10942a16edfb95891debef8cd7
Samandar99 <laziz.go99@gmail.com>
2024-05-18 06:25:58 +0500

    ss

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ['https://chat-react-js.vercel.app', 'http://localhost:5174','http://localhost:5173'],
+        origin: ['https://chat-react-js.vercel.app', 'http://localhost:5174', 'http://localhost:5173'],
         methods: ["GET", "POST"]
     }
 });
@@ -26,14 +26,18 @@ const io = new Server(server, {
 const PORT = process.env.PORT || 5555;
 
 app.use(cors({
-    origin: ['https://chat-react-js.vercel.app', 'http://localhost:5174','http://localhost:5173'],
+    origin: ['https://chat-react-js.vercel.app', 'http://localhost:5174', 'http://localhost:5173'],
     methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 }));
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://chat-react-js.vercel.app', 'http://localhost:5173', 'http://localhost:5174');
+    const allowedOrigins = ['https://chat-react-js.vercel.app', 'http://localhost:5173', 'http://localhost:5174'];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
