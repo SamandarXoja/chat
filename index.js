160b7cd958c947a50cfd7abd5ad8410815ab2559
Samandar99 <laziz.go99@gmail.com>
2024-05-18 05:07:12 +0500

    hello

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(cors({
 }));
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://chat-react-js.vercel.app');
+    res.setHeader('Access-Control-Allow-Origin', 'https://chat-react-js.vercel.app', 'http://localhost:5173', 'http://localhost:5174');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
